441630c134cf25964d33ae14b295b1e9c0c00558
Gregg Wong <griekup@live.com>
2021-08-27 10:48:15 -0500

    Refactor owner list

--- src/views/Owner/Owners.vue
+++ src/views/Owner/Owners.vue
@@ -1,42 +1,63 @@
 <template>
     <div>
-        <ul>
+        <router-link to="/" class="btn btn-primary">Create</router-link>
+        <!-- <ul>
             <li v-for="owner in owners" :key="owner.id">
-                {{owner}}
+                <OwnerCard :owner="owner" />
             </li>
-        </ul>
-        <Fieldset legend="Header">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </Fieldset>
-        
+        </ul> -->
+        <section class="row">
+            <section class="col-md-12">
+                <section class="table-responsive">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>Owner Name</th>
+                                <th>Owner Address</th>
+                                <th>Date of Birth</th>
+                                <th>Details</th>
+                                <th>Update</th>
+                                <th>Delete</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <OwnerRow
+                            v-for="owner in owners"
+                            :key="owner.ownerId"
+                            :owner="owner"
+                            @click.native="onOwnerClicked(owner.ownerId)" />
+                        </tbody>
+                    </table>
+                </section>
+            </section>
+        </section>
     </div>
 </template>
 
 <script>
 import OwnerCard from '@/components/Owner/OwnerCard'
-import Fieldset from 'primevue/fieldset/sfc'
 import { reactive, toRefs, onMounted } from 'vue'
 import {useStore} from 'vuex'
+import OwnerRow from '../../components/Owner/OwnerRow.vue'
 
 export default {
     components: {
         OwnerCard,
-        Fieldset
+        OwnerRow
     },
     setup () {
         const store = useStore();
         const storeName = 'ownerStore';
         const state = reactive({
-            owners: []
+            owners: [],
+            onOwnerClicked(ownerId) {
+                console.log(ownerId);
+            }
         });
        
        onMounted(() => {
            store.dispatch(`${storeName}/getOwners`, null, {root: true}).then((res) => {
                state.owners = res;
-               console.log(res);
            }).catch((err) => {
                // Setup notification service
                console.log(err);
