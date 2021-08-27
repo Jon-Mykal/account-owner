e27900022ef9a064ae794b3e1ec6d540e6dbec6d
Gregg Wong <griekup@live.com>
2021-08-27 10:47:54 -0500

    Add OwnerRow component

---
+++ src/components/Owner/OwnerRow.vue
@@ -0,0 +1,36 @@
+<template>
+    <tr>
+        <td>{{ owner.name }}</td>
+        <td>{{ owner.address }}</td>
+        <td>{{ owner.dateOfBirth }}</td>
+        <td>
+            <Button label="Details" class="p-button"/>
+        </td>
+        <td>
+            <Button label="Edit" class="p-button" />
+        </td>
+        <td>
+            <Button label="Remove"  class="p-button"/>
+        </td>
+    </tr>
+</template>
+
+<script>
+export default {
+    props: {
+        owner: {
+            required: true,
+            type: Object
+        }
+    },
+    name: "OwnerRow",
+    setup () {
+        
+
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
