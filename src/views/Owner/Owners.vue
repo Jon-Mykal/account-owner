0653ee9f4af6c873ce5d95f613d122fe04b994ec
Gregg Wong <griekup@live.com>
2021-08-30 22:02:16 -0500

    Add Confirm Dialog

--- src/views/Owner/Owners.vue
+++ src/views/Owner/Owners.vue
@@ -1,100 +1,130 @@
 <template>
-    <div>
-        <router-link to="/" class="btn btn-primary">Create</router-link>
-        <!-- <ul>
+  <div>
+    <router-link :to="{name: 'OwnerCreate'}" class="btn btn-primary">Create</router-link>
+    <!-- <ul>
             <li v-for="owner in owners" :key="owner.id">
                 <OwnerCard :owner="owner" />
             </li>
         </ul> -->
-        <section class="row">
-            <section class="col-md-12">
-                <section class="table-responsive">
-                    <table class="table table-striped">
-                        <thead>
-                            <tr>
-                                <th>Owner Name</th>
-                                <th>Owner Address</th>
-                                <th>Date of Birth</th>
-                                <th>Details</th>
-                                <th>Update</th>
-                                <th>Delete</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <OwnerRow
-                            v-for="owner in owners"
-                            :key="owner.ownerId"
-                            :owner="owner"
-                            @viewDetails="viewDetails"
-                            @editOwner="editOwner"
-                            @removeOwner="removeOwner" />
-
-                        </tbody>
-                    </table>
-                </section>
-            </section>
+    <ConfirmDialog></ConfirmDialog>
+    <section class="row">
+      <section class="col-md-12">
+        <section class="table-responsive">
+          <table class="table table-striped">
+            <thead>
+              <tr>
+                <th>Owner Name</th>
+                <th>Owner Address</th>
+                <th>Date of Birth</th>
+                <th>Details</th>
+                <th>Update</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              <OwnerRow
+                v-for="owner in owners"
+                :key="owner.ownerId"
+                :owner="owner"
+                @viewDetails="viewDetails"
+                @editOwner="editOwner"
+                @removeOwner="removeOwner"
+              />
+            </tbody>
+          </table>
         </section>
-    </div>
+      </section>
+    </section>
+  </div>
 </template>
 
 <script>
-import OwnerCard from '@/components/Owner/OwnerCard'
-import { reactive, toRefs, onMounted } from 'vue'
-import {useStore} from 'vuex'
-import { useRouter} from 'vue-router';
-import OwnerRow from '../../components/Owner/OwnerRow.vue'
+import OwnerCard from "@/components/Owner/OwnerCard";
+import { reactive, toRefs, onMounted } from "vue";
+import { useStore } from "vuex";
+import { useRouter } from "vue-router";
+import { useConfirm } from "primevue/useconfirm";
+import OwnerRow from "../../components/Owner/OwnerRow.vue";
 
 export default {
-    components: {
-        OwnerCard,
-        OwnerRow
-    },
-    setup () {
-        const store = useStore();
-        const router = useRouter();
-        const storeName = 'ownerStore';
+  components: {
+    OwnerCard,
+    OwnerRow
+  },
+  setup() {
+    try {
+      const store = useStore();
+      const router = useRouter();
+      const confirm = useConfirm();
+      const storeName = "ownerStore";
 
-        const routeData = {
-            name: '',
-            params: null
-        };
-        const state = reactive({
-            owners: [],
-            viewDetails(ownerId) {
-               routeData.name = 'OwnerDetails';
-               routeData.params = { id: ownerId };
-               router.push(routeData);
-            },
-            editOwner(ownerId) {
-               routeData.name = 'OwnerDetails';
-               routeData.params = { id: ownerId };
-                console.log(ownerId);
-            },
-            removeOwner(ownerId) {
-               routeData.name = 'OwnerDetails';
-               routeData.params = { id: ownerId };
-                console.log(ownerId);
+      const routeData = {
+        name: "",
+        params: null,
+      };
+      const state = reactive({
+        owners: [],
+        selectedOwner: null,
+        viewDetails(ownerId) {
+          routeData.name = "OwnerDetails";
+          routeData.params = { id: ownerId };
+          router.push(routeData);
+        },
+        editOwner(ownerId) {
+          routeData.name = "OwnerDetails";
+          routeData.params = { id: ownerId };
+          console.log(ownerId);
+        },
+        removeOwner(ownerId) {
+          confirm.require({
+            message: "Are you sure you want to delete this owner?",
+            header: "Confirmation",
+            icon: "pi pi-exclamation-triangle",
+            accept() {
+              store
+                .dispatch(`${storeName}/deleteOwner`, ownerId, { root: true })
+                .then((owners) => {
+                  confirm.require({
+                    message: "Successfully removed owner",
+                    header: "Status",
+                    icon: "pi pi-success-circle",
+                    re
+                  });
+                })
+                .catch((err) => {
+                  confirm.require({
+                    message: "Failed to removed owner. Please try again.",
+                    header: "Status",
+                    icon: "pi pi-danger-circle",
+                  });
+                  console.log(
+                    `Something went wrong trying to remove owner. ${err}}`
+                  );
+                });
             }
-        });
-       
-       onMounted(() => {
-           store.dispatch(`${storeName}/getOwners`, null, {root: true}).then((res) => {
-               state.owners = res;
-           }).catch((err) => {
-               // Setup notification service
-               console.log(err);
-           });
-       });
-    
-
-        return {
-            ...toRefs(state),
-            onMounted
+          });
         }
-    }
+      });
+
+      onMounted(() => {
+        store
+          .dispatch(`${storeName}/getOwners`, null, { root: true })
+          .then((res) => {
+            state.owners = res;
+          })
+          .catch((err) => {
+            // Setup notification service
+            console.log(err);
+          });
+      });
+
+      return {
+        ...toRefs(state),
+        onMounted,
+      };
+    } catch (err) {}
+  }
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
