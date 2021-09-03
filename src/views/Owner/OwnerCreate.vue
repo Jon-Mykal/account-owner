7428cb019aa91c6fd47a3486edadce56acc7798c
Gregg Wong <griekup@live.com>
2021-09-03 12:58:41 -0500

    Add validations to form

--- src/views/Owner/OwnerCreate.vue
+++ src/views/Owner/OwnerCreate.vue
@@ -11,28 +11,31 @@
         <section class="d-flex justify-content-center">
             <section class="form-wrapper w-25 mt-5 shadow">
                 <h1 class="h3 my-3 py-3 font-weight-normal text-center">Create Owner</h1>
-                <form @submit.prevent class="form-horizontal">
+                <form @submit.prevent="addOwner(!v$.$invalid)" class="form-horizontal">
                 <section class="form-group mb-3 pb-3 px-3">
                     <span class="p-float-label">
-                        <InputText id="ownerName" class="w-100" type="text" v-model="owner.name"/>
-                        <label for="ownerName">Owner's Name</label>
+                        <InputText id="ownerName" :class="{ 'p-invalid': v$.name.$invalid && formSubmitted}" class="w-100" type="text" v-model="v$.name.$model"/>
+                        <label for="ownerName" :class="{ 'p-error': v$.name.$invalid && formSubmitted}">Owner's Name</label>
                     </span>
+                    <small v-if="(v$.name.$invalid && formSubmitted) || v$.name.$pending.$response" class="p-error">{{ v$.name.required.$message.replace('Value', 'Owner\'s Name')}}</small>
                 </section>
                 <section class="form-group mb-3 pb-3 px-3">
                     <span class="p-float-label">
-                        <Calendar id="dateOfBirth" class="w-100" v-model="owner.dateOfBirth" />
-                        <label for="dateOfBirth">Date of birth</label>
+                        <Calendar id="dateOfBirth" :class="{ 'p-invalid': v$.dateOfBirth.$invalid && formSubmitted}" class="w-100" v-model="owner.dateOfBirth" />
+                        <label :class="{ 'p-error': v$.dateOfBirth.$invalid && formSubmitted}" for="dateOfBirth">Date of birth</label>
                     </span>
+                    <small v-if="(v$.dateOfBirth.$invalid && formSubmitted) || v$.dateOfBirth.$pending.$response" class="p-error">{{ v$.dateOfBirth.required.$message.replace('Value', 'Date of birth')}}</small>
                 </section>
                 <section class="form-group mb-3 pb-3 px-3">
                     <span class="p-float-label">
-                        <InputText id="address" class="w-100" v-model="owner.address"/>
-                        <label for="address">Address</label>
+                        <InputText id="address" :class="{ 'p-invalid': v$.address.$invalid && formSubmitted}" class="w-100" v-model="owner.address"/>
+                        <label for="address" :class="{ 'p-error': v$.address.$invalid && formSubmitted }">Address</label>
                     </span>
+                    <small v-if="(v$.address.$invalid && formSubmitted) || v$.address.$pending.$response" class="p-error">{{ v$.address.required.$message.replace('Value', 'Address')}}</small>
                 </section>
                 <section class="d-flex flex-column mb-5 px-3">
-                    <Button label="Add Owner" class="btn btn-primary mb-2" @click="addOwner()"/>
-                    <Button label="Cancel" class="btn btn-outline-secondary" @click="goBack()" />
+                    <Button type="submit" label="Add Owner" class="btn btn-primary mb-2" />
+                    <Button label="Cancel" class="btn btn-outline-secondary p-button-secondary p-button-outlined" @click="goBack()" />
                 </section>
                 </form>
             </section>
@@ -44,6 +47,8 @@
 import { reactive, toRefs } from '@vue/reactivity'
 import { useRouter } from 'vue-router';
 import { useStore } from 'vuex';
+import useVuelidate from '@vuelidate/core';
+import { required } from '@vuelidate/validators';
 export default {
     name: "OwnerCreate",
     setup () {
@@ -55,12 +60,18 @@ export default {
                 statusMessage: "",
                 showPopup: false,
                 isSuccessful: false,
+                formSubmitted: false,
                 owner: {
                     name: "",
                     dateOfBirth: "",
                     address: ""
                 },
-                addOwner() {
+                addOwner(isFormValid) {
+                    this.formSubmitted = true;
+                    if (!isFormValid) {
+                        return;
+                    }
+                    
                     store.dispatch(`${storeName}/addOwner`, this.owner).then((res) => {
                         this.statusMessage = "Owner successfully created!";
                          this.showPopup = true;
@@ -83,8 +94,15 @@ export default {
                     }
                 }
             });
+ 
+            const rules = {
+                name: { required },
+                dateOfBirth: { required },
+                address: { required }
+            }
+            const v$ = useVuelidate(rules, state);
 
-            return { ...toRefs(state) };
+            return { ...toRefs(state), v$ };
         } catch (error) {
             console.log(`Something went wrong. See: ${error}`);
         }
