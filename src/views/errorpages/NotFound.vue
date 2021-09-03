572589dd0fe028660b54fe5bf3ef9a220f24e85a
Gregg Wong <griekup@live.com>
2021-09-02 22:52:55 -0500

    Update NotFound component

--- src/views/errorpages/NotFound.vue
+++ src/views/errorpages/NotFound.vue
@@ -1,12 +1,20 @@
 <template>
     <div>
-        <p>404. Unfortunately we don't have what you were looking for.</p>
+        <h1>Oops!</h1>
+        <p>The {{resource}} you're looking for is not here.</p>
         <router-link :to="{name: 'Home'}">Go to home</router-link>
     </div>
 </template>
 
 <script>
-
+    export default {
+        props: {
+            resource: {
+                type: String,
+                required: true
+            }
+        }
+    }
 </script>
 
 <style scoped>
