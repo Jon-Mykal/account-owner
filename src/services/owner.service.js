7dc408d34a546aa7401e4c21b93f096512d14d95
Gregg Wong <griekup@live.com>
2021-09-02 22:53:30 -0500

    Add timeout to requests

--- src/services/owner.service.js
+++ src/services/owner.service.js
@@ -1,30 +1,31 @@
 import axios from 'axios'
 
 const RESOURCE_NAME = '/owners';
+const config = { timeout: 5000 }
 
 const OwnerService = {
     getAll() {
-        return axios.get(RESOURCE_NAME);
+        return axios.get(RESOURCE_NAME, config);
     },
 
     get(id){
-        return axios.get(`${RESOURCE_NAME}/${id}`);
+        return axios.get(`${RESOURCE_NAME}/${id}`, config);
     },
 
     getAccounts(id) {
-        return axios.get(`${RESOURCE_NAME}/${id}/accounts`)
+        return axios.get(`${RESOURCE_NAME}/${id}/accounts`, config)
     },
 
     create(data) {
-        return axios.post(RESOURCE_NAME, data);
+        return axios.post(RESOURCE_NAME, data, config);
     },
 
     update(id, data) {
-        return axios.put(`${RESOURCE_NAME}/${id}`, data);
+        return axios.put(`${RESOURCE_NAME}/${id}`, data, config);
     },
 
     delete(id) {
-        return axios.delete(`${RESOURCE_NAME}/${id}`);
+        return axios.delete(`${RESOURCE_NAME}/${id}`, config);
     }
 }
 
