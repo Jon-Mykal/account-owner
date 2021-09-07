6cb6eea06231067d2169052428f0d0ae8b2b31f2
Gregg Wong <griekup@live.com>
2021-09-06 22:06:53 -0500

    Implement VeeValidate

--- src/views/Owner/OwnerEdit.vue
+++ src/views/Owner/OwnerEdit.vue
@@ -15,27 +15,27 @@
         <section class="d-flex justify-content-center">
             <section class="form-wrapper w-25 mt-5 shadow">
                 <h1 class="h3 my-3 py-3 font-weight-normal text-center">Edit Owner</h1>
-                <form @submit.prevent="editOwner(!v$.$invalid)" class="form-horizontal">
+                <form @submit.prevent="editOwner(!false)" class="form-horizontal">
                 <section class="form-group mb-3 pb-3 px-3">
                     <span class="p-float-label">
-                        <InputText id="ownerName" :class="{ 'p-invalid': v$.name.$invalid && formSubmitted}" class="w-100" type="text" v-model="v$.name.$model"/>
-                        <label for="ownerName" :class="{ 'p-error': v$.name.$invalid && formSubmitted}">Owner's Name</label>
+                        <InputText id="ownerName" :class="{ 'p-invalid': nameVal.errorMessage && formSubmitted}" class="w-100" type="text"  v-model="nameVal.value" />
+                        <label for="ownerName" :class="{ 'p-error': nameVal.errorMessage && formSubmitted }">Owner's Name</label>
                     </span>
-                    <small v-if="(v$.name.$invalid && formSubmitted) || v$.name.$pending.$response" class="p-error">{{ v$.name.required.$message.replace('Value', 'Owner\'s Name')}}</small>
+                    <small v-if="nameVal.errorMessage && formSubmitted" class="p-error">{{ nameVal.errorMessage }}</small>
                 </section>
                 <section class="form-group mb-3 pb-3 px-3">
                     <span class="p-float-label">
-                        <Calendar id="dateOfBirth" :class="{ 'p-invalid': v$.dateOfBirth.$invalid && formSubmitted}" class="w-100" v-model="v$.dateOfBirth.$model" />
-                        <label :class="{ 'p-error': v$.dateOfBirth.$invalid && formSubmitted}" for="dateOfBirth">Date of birth</label>
+                        <Calendar id="dateOfBirth" :class="{ 'p-invalid': formSubmitted }" class="w-100" v-model="dateOfBirthVal.value" />
+                        <label :class="{ 'p-error': formSubmitted }" for="dateOfBirth">Date of birth</label>
                     </span>
-                    <small v-if="(v$.dateOfBirth.$invalid && formSubmitted) || v$.dateOfBirth.$pending.$response" class="p-error">{{ v$.dateOfBirth.required.$message.replace('Value', 'Date of birth')}}</small>
+                    <small v-if="dateOfBirthVal.errorMessage && formSubmitted" class="p-error">{{dateOfBirthVal.errorMessage}}</small>
                 </section>
                 <section class="form-group mb-3 pb-3 px-3">
                     <span class="p-float-label">
-                        <InputText id="address" :class="{ 'p-invalid': v$.address.$invalid && formSubmitted}" class="w-100" v-model="v$.address.$model"/>
-                        <label for="address" :class="{ 'p-error': v$.address.$invalid && formSubmitted }">Address</label>
+                        <InputText id="address" :class="{ 'p-invalid': addressVal.errorMessage && formSubmitted}" class="w-100" v-model="addressVal.value"/>
+                        <label for="address" :class="{ 'p-error': addressVal.errorMessage && formSubmitted }">Address</label>
                     </span>
-                    <small v-if="(v$.address.$invalid && formSubmitted) || v$.address.$pending.$response" class="p-error">{{ v$.address.required.$message.replace('Value', 'Address')}}</small>
+                    <small v-if="addressVal.errorMessage && formSubmitted" class="p-error">Address errors</small>
                 </section>
                 <section class="d-flex flex-column mb-5 px-3">
                     <Button type="submit" label="Edit Owner" class="btn btn-primary mb-2" />
@@ -65,6 +65,8 @@ import { required } from '@vuelidate/validators';
 import NetworkIssue from '../../views/errorpages/NetworkIssue.vue';
 import NotFound from '../../views/errorpages/NotFound.vue';
 import { usePageLoading } from '@/composables/usePageLoading';
+import { useField, useForm } from 'vee-validate';
+import * as yup from 'yup';
 
 export default {
     name: "OwnerEdit",
@@ -83,7 +85,25 @@ export default {
             const store = useStore();
             const router = useRouter();
             const storeName = 'ownerStore';
-            const pageLoader = usePageLoading();    
+            const pageLoader = usePageLoading();  
+            
+            const valSchema = yup.object({
+                name: yup.string().required().label('Owner\'s name'),
+                dateOfBirth: yup.date().required().label('Date of birth'),
+                address: yup.string().required().label('Address')
+            });
+
+            const formConfig = useForm({
+                validationSchema: valSchema
+            });
+
+            const validations = reactive({
+                nameVal: useField('name'),
+                dateOfBirthVal: useField('dateOfBirth'),
+                addressVal: useField('address')
+            });
+
+
             const state = reactive({
                 statusMessage: "",
                 showPopup: false,
@@ -97,13 +117,16 @@ export default {
                     dateOfBirth: "",
                     address: ""
                 },
+                testInput(event) {
+                    console.log(event);
+                },
                 editOwner(isFormValid) {
                     this.formSubmitted = true;
-
-console.log(v$.name);
                     if (!isFormValid) {
                         return;
                     }
+
+                    return;
                     
                     this.loading = true;
                     store.dispatch(`${storeName}/editOwner`, {id: props.id, owner: this.owner}).then((res) => {
@@ -150,6 +173,12 @@ console.log(v$.name);
                     state.loading = false;
                     state.owner = res;
                     state.ownerFound = true;
+                    // formConfig.setValues({
+                    //     name: state.owner.name,
+                    //     dateOfBirth: state.owner.dateOfBirth,
+                    //     address: state.owner.address
+                    // })
+                    formConfig.setValues(state.owner);
                 }).catch((err) => {
                     pageLoader.closeLoading();
                     state.loading = false;
@@ -165,17 +194,9 @@ console.log(v$.name);
                     console.log(`Something went wrong. See: ${err}`);
                 });
             });
-
-            const rules = computed(() => {
-                return {
-                name: { required, $model: 'rwer' },
-                dateOfBirth: { required },
-                address: { required }
-                }
-             });
-            const v$ = useVuelidate(rules, state, { $autoDirty: true });
-
-            return { ...toRefs(state), onBeforeMount, v$ };
+             
+            
+            return { ...toRefs(state), ...toRefs(validations), onBeforeMount, formConfig};
 
         } catch (error) {
             console.log(`Something went wrong. See: ${error}`);
