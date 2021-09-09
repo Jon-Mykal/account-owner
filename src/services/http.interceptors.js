bc31a1e403e6e749cf2c0a8d3ccdc3c97ca07f9d
Gregg Wong <griekup@live.com>
2021-09-09 13:51:43 -0500

    Setup HttpInterceptors

---
+++ src/services/http.interceptors.js
@@ -0,0 +1,15 @@
+import { AuthService } from "./auth.service";
+
+const authSvc = new AuthService();
+
+export const interceptorsInit = (axiosInstance)  => {
+    axiosInstance.interceptors.request.use((options) => {
+        const jwtToken = authSvc.getToken();
+
+        if (jwtToken) {
+            options.headers["Authorization"] = `Bearer ${jwtToken}`;
+        }
+
+        return options;
+    });
+}   
