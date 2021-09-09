9a42945e5edef17411b8d72fe34900e621c94f1b
Gregg Wong <griekup@live.com>
2021-09-09 13:54:15 -0500

    Refactor owner service

--- src/services/owner.service.js
+++ src/services/owner.service.js
@@ -1,31 +1,31 @@
-import axios from 'axios'
+import {api} from './apiClient'
 
 const RESOURCE_NAME = '/owners';
 const config = { timeout: 5000 }
 
 const OwnerService = {
     getAll() {
-        return axios.get(RESOURCE_NAME, config);
+        return api.get(RESOURCE_NAME, config);
     },
 
     get(id){
-        return axios.get(`${RESOURCE_NAME}/${id}`, config);
+        return api.get(`${RESOURCE_NAME}/${id}`, config);
     },
 
     getAccounts(id) {
-        return axios.get(`${RESOURCE_NAME}/${id}/accounts`, config)
+        return api.get(`${RESOURCE_NAME}/${id}/accounts`, config)
     },
 
     create(data) {
-        return axios.post(RESOURCE_NAME, data, config);
+        return api.post(RESOURCE_NAME, data, config);
     },
 
     update(id, data) {
-        return axios.put(`${RESOURCE_NAME}/${id}`, data, config);
+        return api.put(`${RESOURCE_NAME}/${id}`, data, config);
     },
 
     delete(id) {
-        return axios.delete(`${RESOURCE_NAME}/${id}`, config);
+        return api.delete(`${RESOURCE_NAME}/${id}`, config);
     }
 }
 
