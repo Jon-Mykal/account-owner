0fdd3481da4f7dbe1b29527d907d0331eebb25b0
Gregg Wong <griekup@live.com>
2021-09-09 14:03:30 -0500

    Fix readonly error

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -4,7 +4,7 @@ import { interceptorsInit } from "./http.interceptors";
 
 const baseURL = process.env.VUE_APP_API_ENDPOINT
 
-const api = axios.create({ baseURL});
+let api = axios.create({ baseURL});
 api = interceptorsInit(api);
 
 export default api;
