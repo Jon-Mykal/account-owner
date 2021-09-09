429262f76db8830a22f593f4ec218b7f72b42f3d
Gregg Wong <griekup@live.com>
2021-09-09 13:47:43 -0500

    Create AuthGuard for protected routes

---
+++ src/router/auth.guard.js
@@ -0,0 +1,19 @@
+import store  from "../store/index";
+
+const storeName = 'authStore';
+export const authGuard = (to, from, next) => {
+    // Get all routes requiring auth.
+    const authRequired = to.matched.some((record) => record.meta.requiresAuth);
+    console.log(authRequired);
+    if (authRequired) {
+        if (store.getters[`${storeName}/isAuthenticated`]) {
+            next();
+            return;
+        }
+        next({
+            path: '/account/login',
+            query: { returnUrl: to.fullPath }
+        });
+    }
+    next();
+};
