140343bfc0363382b8fef9ceddcf4a292708f6e6
Gregg Wong <griekup@live.com>
2021-09-09 13:54:48 -0500

    Add additional methods to auth service

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,4 +1,5 @@
-import axios from 'axios'
+import {api} from './apiClient'
+import * as jwt from 'jsonwebtoken'
 
 const RESOURCE_NAME = '/account';
 const config = { timeout: 5000 }
@@ -11,12 +12,43 @@ export class AuthService {
     registerUser(route, userRegDto) {
         const completeRoute = `${RESOURCE_NAME}/${route}`;
 
-        return axios.post(completeRoute, userRegDto);
+        return api.post(completeRoute, userRegDto);
     }
 
     loginUser(route, userAuthDto) {
         const completeRoute = `${RESOURCE_NAME}/${route}`;
-        return axios.post(completeRoute, userAuthDto);
+        return api.post(completeRoute, userAuthDto);
+    }
+
+    getToken() {
+        return localStorage.getItem("token");
+    }
+
+    logoutUser() {
+        localStorage.clear();
+        location.reload();
+    }
+
+    isTokenValid() {
+        let isValid = false;
+        try {
+            const token = localStorage.getItem("token");
+            if (!token) {
+                return isValid;
+            }
+
+            const decoded = jwt.decode(token);
+            const expiresAt = decoded.exp * 1000;
+            const dateNow = Date.now();
+
+            isValid = dateNow <= expiresAt;
+
+            return isValid;
+        } catch (error) {
+            console.log(`Something went wrong with AuthService.isTokenValid(): ${error}`);
+        }
+
+        return isValid;
     }
 
 
