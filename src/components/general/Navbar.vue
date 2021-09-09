13c8bee35d077e2558ee56e9551e1915c035a5e0
Gregg Wong <griekup@live.com>
2021-09-08 22:02:10 -0500

    Update navbar with Auth Links

--- src/components/general/Navbar.vue
+++ src/components/general/Navbar.vue
@@ -15,19 +15,52 @@
         <span class="navbar-toggler-icon"></span>
       </button>
       <section class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav">
-              <li class="nav-item">
-                  <router-link :to="{name: 'OwnersList'}" class="nav-link">Owners</router-link>
+          <ul v-if="!isLoggedIn" class="navbar-nav">
+            <li class="nav-item">
+              <router-link :to="{name: 'Register'}" class="nav-link">Register</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link :to="{ name: 'Login' }" class="nav-link">Login</router-link>
             </li>
           </ul>
+          <ul class="navbar-nav" v-if="isLoggedIn">
+            <li class="nav-item">
+                  <router-link :to="{name: 'OwnersList'}" class="nav-link">Owners</router-link>
+            </li>
+            <li class="nav-item">
+                  <a href= "javascript:void(0)" @click="logout()" class="nav-link">Logout</a>
+            </li>
+          </ul>
+          <span>{{isLoggedIn}}</span>
       </section>
     </section>
   </nav>
 </template>
 
 <script>
+import { computed, onMounted, ref, watchEffect } from 'vue';
+import { useStore } from 'vuex';
+
 export default {
-  setup() {}
+  setup() {
+    const storeName = 'authStore';
+    const store = useStore();
+
+    const logout = () => {
+      store.dispatch(`${storeName}/logout`);
+    }
+
+    const isLoggedIn = computed(() => {
+      return store.getters[`${storeName}/isAuthenticated`];
+    });
+
+
+    onMounted(() => {
+      //isLoggedIn.value = store.getters[`${storeName}/isAuthenticated`];
+    });
+
+    return { isLoggedIn, logout, onMounted }
+  }
 }
 </script>
 
