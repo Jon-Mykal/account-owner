f6daba146024da51f3424f99b93c3a203be5be4c
Gregg Wong <griekup@live.com>
2021-09-14 12:29:21 -0500

    Refactor Google flow and UI

--- src/views/account/Login.vue
+++ src/views/account/Login.vue
@@ -24,23 +24,40 @@
                         <Password id="password" :class="{ 'p-invalid': false}" class="w-100" inputClass="w-100" v-model="user.password" toggleMask></Password>
                         <label :class="{ 'p-error': false}" for="password">Password</label>
                     </span>
+                    <section class="p-field pt-1 d-flex">
+                        <span class="p-field fst-italic pt-1">
+                            <router-link :to="{ name: 'ForgotPassword'}">Forgot Password</router-link>
+                        </span>
+                    </section>
                     <!-- <small v-if="(v$.dateOfBirth.$invalid && formSubmitted) || v$.dateOfBirth.$pending.$response" class="p-error">{{ v$.dateOfBirth.required.$message.replace('Value', 'Date of birth')}}</small> -->
                 </section>
                 <section class="d-flex flex-column mb-5 px-3">
+                    <section class="p-field pt-2">
+                        <p class="text-center">
+                            Don't have an account? <router-link :to="{name: 'Register'}">Sign Up</router-link>
+                        </p>
+                    </section>
                     <Button type="submit" label="Login" class="btn btn-primary mb-2" />
-                    <Button label="Cancel" class="btn btn-outline-secondary p-button-secondary p-button-outlined" @click="goBack()" />
+                    <Divider align="center">
+                        <strong>Or</strong>
+                    </Divider>
+                    <Button label="Sign in with Google" class="btn btn-primary" @click="signInWithGoogle()"/>
                 </section>
                 </form>
             </section>
         </section>
+        <!-- <section class="pt-2">
+            <Button label="Sign in with Google" @click="signInWithGoogle()"/>
+        </section> -->
     </div>
 </template>
 
 <script>
-import { reactive, toRefs } from 'vue';
+import { reactive, toRefs, inject } from 'vue';
 import { useRoute, useRouter } from 'vue-router'
 import { UserAuthDto } from '../../models/userAuthDto'
 import { useStore } from 'vuex';
+
 export default {
     setup () {
         const route = useRoute();
@@ -49,7 +66,7 @@ export default {
         const store = useStore();
         let returnUrl = route.query["returnUrl"] || '/';
         
-
+const gAuth = inject('googleAuth');
         const state = reactive({
             user: new UserAuthDto(),
             showPopup: false,
@@ -67,6 +84,33 @@ export default {
             },
             closePopup() {
 
+            },
+            signInWithGoogle() {
+                store.dispatch(`${storeName}/signInWithGoogle`, 'externallogin');
+                google.accounts.id.prompt((notification) => {   
+                    console.log('notification', notification);
+                    if (notification.isDisplayMoment()) {
+                        console.log('Display moment occurring');
+                    }
+
+                    if (notification.isSkippedMoment()) {
+                        console.log(`Notification skipped because: ${notification.getSkippedReason()}`);
+                    }
+
+                    if (notification.isDismissedMoment()) {
+                        console.log(`Notification dismissed because: ${notification.getDismissedReason()}`);
+                    }
+
+                    if (notification.isDisplayed()) {
+                        console.log("UI displayed");
+                    }
+                    else if (notification.isNotDisplayed()) {
+                        console.log(`UI not displayed because: ${notification.getNotDisplayedReason()}`);
+                    }
+
+                });
+                
+                //console.log(gAuth);
             }
         });
         return {...toRefs(state)}
