1e44904807d3693d5daa78707413b3e10cb8e921
Gregg Wong <griekup@live.com>
2021-09-27 22:27:16 -0500

    Add Refresh token check to auth guard

--- src/router/auth.guard.js
+++ src/router/auth.guard.js
@@ -1,21 +1,36 @@
 import store  from "../store/index";
+import { AuthService } from "../services/auth.service";
 
+const authSvc = new AuthService();
 const storeName = 'authStore';
-export const authGuard = (to, from, next) => {
+export const authGuard = async (to, from, next) => {
     // Get all routes requiring auth.
     const authRequired = to.matched.some((record) => record.meta.requiresAuth);
+    const isAuthenticated = store.getters[`${storeName}/isAuthenticated`];
+    // Always attempt to send the refresh.
+    
     console.log(authRequired);
-    if (authRequired) {
-        if (store.getters[`${storeName}/isAuthenticated`]) {
+    if (authRequired && isAuthenticated) {
+            await store.dispatch(`${storeName}/tryTokenRefresh`);
             next();
             return;
-        }
+    }
+    else if (authRequired && !isAuthenticated) {
+        // Force login
         next({
             path: '/account/login',
             query: { returnUrl: to.fullPath }
         });
+        return;
+    }
+    else if (!authRequired && isAuthenticated) {
+        // Continue to refresh as long as user is logged in
+        await store.dispatch(`${storeName}/tryTokenRefresh`);
+        next();
+        return;
     }
     else {
+        // Don't attempt refresh
         next();
     }
     
