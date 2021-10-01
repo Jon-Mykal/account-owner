8a34fc98f41aa3f4452c4d0eb79048111d2b23c4
Gregg Wong <griekup@live.com>
2021-10-01 13:21:41 -0500

    Fix UI, Fix Datepicker

--- src/views/Owner/OwnerCreate.vue
+++ src/views/Owner/OwnerCreate.vue
@@ -9,31 +9,37 @@
             </template>
         </Dialog>
         <section class="d-flex justify-content-center">
-            <section class="form-wrapper w-25 mt-5 shadow">
+            <section class="form-wrapper col-md-7 col-sm-9 col-12 col-lg-5 mt-5 shadow">
                 <h1 class="h3 my-3 py-3 font-weight-normal text-center">Create Owner</h1>
-                <form @submit.prevent="addOwner(!v$.$invalid)" class="form-horizontal">
-                <section class="form-group mb-3 pb-3 px-3">
+                <form @submit.prevent="addOwner(!v$.$invalid)" class="form-horizontal px-md-4 mx-2">
+                <section class="form-group mb-3 pb-3">
                     <span class="p-float-label">
                         <InputText id="ownerName" :class="{ 'p-invalid': v$.name.$invalid && formSubmitted}" class="w-100" type="text" v-model="v$.name.$model"/>
                         <label for="ownerName" :class="{ 'p-error': v$.name.$invalid && formSubmitted}">Owner's Name</label>
                     </span>
                     <small v-if="(v$.name.$invalid && formSubmitted) || v$.name.$pending.$response" class="p-error">{{ v$.name.required.$message.replace('Value', 'Owner\'s Name')}}</small>
                 </section>
-                <section class="form-group mb-3 pb-3 px-3">
+                <section class="form-group mb-3 pb-3">
                     <span class="p-float-label">
-                        <Calendar id="dateOfBirth" :class="{ 'p-invalid': v$.dateOfBirth.$invalid && formSubmitted}" class="w-100" v-model="owner.dateOfBirth" />
+                        <Calendar id="dateOfBirth" :class="{ 'p-invalid': v$.dateOfBirth.$invalid && formSubmitted}" class="w-100" v-model="owner.dateOfBirth" 
+                        :showButtonBar="true"
+                        :monthNavigator="true"
+                        :yearNavigator="true"
+                        :yearRange="dateRange"
+                        :minDate="minDate"
+                        :maxDate="maxDate" />
                         <label :class="{ 'p-error': v$.dateOfBirth.$invalid && formSubmitted}" for="dateOfBirth">Date of birth</label>
                     </span>
                     <small v-if="(v$.dateOfBirth.$invalid && formSubmitted) || v$.dateOfBirth.$pending.$response" class="p-error">{{ v$.dateOfBirth.required.$message.replace('Value', 'Date of birth')}}</small>
                 </section>
-                <section class="form-group mb-3 pb-3 px-3">
+                <section class="form-group mb-3 pb-3">
                     <span class="p-float-label">
                         <InputText id="address" :class="{ 'p-invalid': v$.address.$invalid && formSubmitted}" class="w-100" v-model="owner.address"/>
                         <label for="address" :class="{ 'p-error': v$.address.$invalid && formSubmitted }">Address</label>
                     </span>
                     <small v-if="(v$.address.$invalid && formSubmitted) || v$.address.$pending.$response" class="p-error">{{ v$.address.required.$message.replace('Value', 'Address')}}</small>
                 </section>
-                <section class="d-flex flex-column mb-5 px-3">
+                <section class="d-flex flex-column mb-5">
                     <Button type="submit" label="Add Owner" class="btn btn-primary mb-2" />
                     <Button label="Cancel" class="btn btn-outline-secondary p-button-secondary p-button-outlined" @click="goBack()" />
                 </section>
@@ -61,6 +67,9 @@ export default {
                 showPopup: false,
                 isSuccessful: false,
                 formSubmitted: false,
+                dateRange: "1900:2050",
+                minDate: new Date(),
+                maxDate: new Date(),
                 owner: {
                     name: "",
                     dateOfBirth: "",
@@ -95,6 +104,9 @@ export default {
                 }
             });
  
+            // Configure date picker
+            state.minDate.setMonth(0);
+            state.minDate.setFullYear(1900);
             const rules = {
                 name: { required },
                 dateOfBirth: { required },
