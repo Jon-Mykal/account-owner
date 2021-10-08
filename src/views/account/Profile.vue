fb504cf4dcf1bc00c03bfb5309a0e146f4712019
Gregg Wong <griekup@live.com>
2021-10-08 09:24:46 -0500

    Add Profile Page

---
+++ src/views/account/Profile.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+import { usePageLoading } from '@/composables/usePageLoading';
+import { onMounted } from 'vue';
+export default {
+    setup () {
+        const pageLoader = usePageLoading();
+        onMounted(() => {
+            pageLoader.closeLoading();
+        })
+        return { onMounted }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
